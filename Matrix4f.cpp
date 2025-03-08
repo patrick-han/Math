0ee1c10e4a0aadc5aaa5a190f30b199485c251d7
Patrick Han <47380073+patrick-han@users.noreply.github.com>
2025-03-08 15:01:31 -0800

    whitespace

--- Matrix4f.cpp
+++ Matrix4f.cpp
@@ -14,7 +14,7 @@ Matrix4f::Matrix4f() {
     Identity();
 }
 Matrix4f::Matrix4f(
-        float _m00, float _m01, float _m02, float _m03
+      float _m00, float _m01, float _m02, float _m03
     , float _m10, float _m11, float _m12, float _m13
     , float _m20, float _m21, float _m22, float _m23
     , float _m30, float _m31, float _m32, float _m33)
@@ -26,7 +26,7 @@ Matrix4f::Matrix4f(
 
 Matrix4f Matrix4f::FromColumns(const Vector4f& c1, const Vector4f& c2, const Vector4f& c3, const Vector4f& c4) {
     return Matrix4f(
-            c1.x, c2.x, c3.x, c4.x
+          c1.x, c2.x, c3.x, c4.x
         , c1.y, c2.y, c3.y, c4.y
         , c1.z, c2.z, c3.z, c4.z
         , c1.w, c2.w, c3.w, c4.w
@@ -35,7 +35,7 @@ Matrix4f Matrix4f::FromColumns(const Vector4f& c1, const Vector4f& c2, const Vec
 
 Matrix4f Matrix4f::FromRows(const Vector4f& r1, const Vector4f& r2, const Vector4f& r3, const Vector4f& r4) {
     return Matrix4f(
-            r1.x, r1.y, r1.z, r1.w
+          r1.x, r1.y, r1.z, r1.w
         , r2.x, r2.y, r2.z, r2.w
         , r3.x, r3.y, r3.z, r3.w
         , r4.x, r4.y, r4.z, r4.w
@@ -44,7 +44,7 @@ Matrix4f Matrix4f::FromRows(const Vector4f& r1, const Vector4f& r2, const Vector
 
 Matrix4f Matrix4f::MakeScale(float s) {
     return Matrix4f(
-            s, 0.0f, 0.0f, 0.0f
+          s, 0.0f, 0.0f, 0.0f
         , 0.0f, s, 0.0f, 0.0f
         , 0.0f, 0.0f, s, 0.0f
         , 0.0f, 0.0f, 0.0f, 1.0f
@@ -53,7 +53,7 @@ Matrix4f Matrix4f::MakeScale(float s) {
 
 Matrix4f Matrix4f::MakeScale(float sx, float sy, float sz) {
     return Matrix4f(
-            sx, 0.0f, 0.0f, 0.0f
+          sx, 0.0f, 0.0f, 0.0f
         , 0.0f, sy, 0.0f, 0.0f
         , 0.0f, 0.0f, sz, 0.0f
         , 0.0f, 0.0f, 0.0f, 1.0f
@@ -62,7 +62,7 @@ Matrix4f Matrix4f::MakeScale(float sx, float sy, float sz) {
 
 Matrix4f Matrix4f::MakeScale(const Vector3f& scale) {
     return Matrix4f(
-            scale.x, 0.0f,    0.0f,    0.0f
+          scale.x, 0.0f,    0.0f,    0.0f
         , 0.0f,    scale.y, 0.0f,    0.0f
         , 0.0f,    0.0f,    scale.z, 0.0f
         , 0.0f,    0.0f,    0.0f,    1.0f
@@ -71,7 +71,7 @@ Matrix4f Matrix4f::MakeScale(const Vector3f& scale) {
 
 Matrix4f Matrix4f::MakeTranslate(float tx, float ty, float tz) {
     return Matrix4f(
-            1.0f, 0.0f, 0.0f, tx
+          1.0f, 0.0f, 0.0f, tx
         , 0.0f, 1.0f, 0.0f, ty
         , 0.0f, 0.0f, 1.0f, tz
         , 0.0f, 0.0f, 0.0f, 1.0f
@@ -80,7 +80,7 @@ Matrix4f Matrix4f::MakeTranslate(float tx, float ty, float tz) {
 
 Matrix4f Matrix4f::MakeTranslate(const Vector3f& translate) {
     return Matrix4f(
-            1.0f, 0.0f, 0.0f, translate.x
+          1.0f, 0.0f, 0.0f, translate.x
         , 0.0f, 1.0f, 0.0f, translate.y
         , 0.0f, 0.0f, 1.0f, translate.z
         , 0.0f, 0.0f, 0.0f, 1.0f
@@ -93,7 +93,7 @@ Matrix4f Matrix4f::MakeRotateX(float rads) {
     float s = std::sin(rads);
 
     return Matrix4f(
-            1.0f, 0.0f, 0.0f, 0.0f
+          1.0f, 0.0f, 0.0f, 0.0f
         , 0.0f, c,    -s,   0.0f
         , 0.0f, s,     c,   0.0f
         , 0.0f, 0.0f, 0.0f, 1.0f
@@ -104,7 +104,7 @@ Matrix4f Matrix4f::MakeRotateY(float rads) {
     float s = std::sin(rads);
 
     return Matrix4f(
-            c,    0.0f, s,    0.0f
+          c,    0.0f, s,    0.0f
         , 0.0f, 1.0f, 0.0f, 0.0f
         , -s,   0.0f, c,    0.0f
         , 0.0f, 0.0f, 0.0f, 1.0f
@@ -115,7 +115,7 @@ Matrix4f Matrix4f::MakeRotateZ(float rads) {
     float s = std::sin(rads);
 
     return Matrix4f(
-            c,    -s,   0.0f, 0.0f
+          c,    -s,   0.0f, 0.0f
         , s,     c,   0.0f,   0.0f
         , 0.0f, 0.0f, 1.0f,   0.0f
         , 0.0f, 0.0f, 0.0f, 1.0f
@@ -124,7 +124,7 @@ Matrix4f Matrix4f::MakeRotateZ(float rads) {
 
 Matrix4f Matrix4f::Transposed() {
     return Matrix4f(
-            m00, m10, m20, m30
+          m00, m10, m20, m30
         , m01, m11, m21, m31
         , m02, m12, m22, m32
         , m03, m13, m23, m33
