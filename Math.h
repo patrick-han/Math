8edae318ba4e31e4bb579ab86c391885ab44cd64
Patrick Han <47380073+patrick-han@users.noreply.github.com>
2026-03-20 16:23:14 -0700

    add clampf

--- Math.h
+++ Math.h
@@ -1,6 +1,20 @@
 #pragma once
 #include <numbers>
 
-inline float deg2rad(float degrees) {
+inline float deg2rad(float degrees)
+{
     return static_cast<float>(degrees * (std::numbers::pi / 180.0f));
+}
+
+inline float clampf(float x, float min, float max)
+{
+    if (x < min)
+    {
+        return min;
+    }
+    if (x > max)
+    {
+        return max;
+    }
+    return x;
 }
